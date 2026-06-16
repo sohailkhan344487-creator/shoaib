2414c8b26dd8ca883a1c5c6bfaf95f4c533b2066
sohailkhan344487-creator <sohailkhan344487@gmail.com>
2026-06-16 14:56:56 +0530

    initial commit

---
+++ src/main/java/vy23cs041/cs407/App.java
@@ -0,0 +1,13 @@
+package vy23cs041.cs407;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
